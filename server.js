778e0dae4e1ee340bdc55448a044be91473cd581
Przemek Burczyk <przemek.burczyk@sigmapoint.co>
2014-03-30 22:30:21 +0200

    Implemented admin's panel prototype (not yet entirely functional).

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var app = require('express')()
 
 server.listen(3000);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+app.get('/client', function (req, res) {
+  	res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/admin', function (req, res) {
+	res.sendfile(__dirname + '/admin_panel.html');
 });
 
 var tasks = require("./tasks.js");
@@ -13,17 +17,22 @@ var tasks = require("./tasks.js");
 var Dispatcher = require("./task_dispacher.js");
 var dispatcher = new Dispatcher();
 
-io.sockets.on('connection', function (socket) {
-	console.log("new user connected")
-
-	if(io.sockets.clients().length == 3) {
+io.of('/client').on('connection', function (socket) {
+	console.log("client connected");
+	if(io.of('/client').clients().length == 3) {
 		console.log("starting task")
-	 	dispatcher.dispatch_tasks(tasks.find_primes_in_range, 1, 100, io.sockets.clients());
+	 	dispatcher.dispatch_tasks(tasks.find_primes_in_range, 1, 100, io.of('/client').clients());
 	 	dispatcher.on('completed', function(results, a) {
-	 		console.log("Results arrivers: " + JSON.stringify(results) +"!")
- 		});
-	 }
+ 			console.log("Results arrivers: " + JSON.stringify(results) +"!")
+		});
+ 	}
 });
 
+io.of('/admin').on('connection', function (socket) {
+	console.log("admin connected");
+});
 
+io.of('/admin').on('command', function (data) {
+	console.log('command executed');
+});
 
