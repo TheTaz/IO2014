e688e74b875ae0f96b84f45dc84ed83bfe460fab
Maciek Wątroba <maciek.bb@gmail.com>
2014-04-05 17:58:00 +0200

    fix specs formatting

--- spec/connection_manager_spec.js
+++ spec/connection_manager_spec.js
@@ -1,22 +1,17 @@
 describe("ConnectionManager", function() {
-	var sockets = jasmine.createSpyObj('sockets', ['on', 'clients']);
+    var sockets = jasmine.createSpyObj('sockets', ['on', 'clients']);
+    var ConnectionManager = require("../src/connection_manager.js");
+    var connectionManager = new ConnectionManager(sockets);
 
-  
-  it("returns list of connected clients", function() {
-		var ConnectionManager = require("../src/connection_manager.js");
-		var connectionManager = new ConnectionManager(sockets);
+    it("returns list of connected clients", function() {
+        connectionManager.connected_clients();
+        expect(sockets.clients).toHaveBeenCalled();
+    });
 
-		connectionManager.connected_clients();
-		expect(sockets.clients).toHaveBeenCalled();
-  });
+    it("executes function on connection", function() {
+        var callback = jasmine.createSpy('callback')
+        connectionManager.on_connection(callback);
 
-	it("executes function on connection", function() {
-		var ConnectionManager = require("../src/connection_manager.js");
-		var connectionManager = new ConnectionManager(sockets);
-
-		var callback = jasmine.createSpy('callback')
-		connectionManager.on_connection(callback);
-
-		expect(sockets.on).toHaveBeenCalled();
-	});
+        expect(sockets.on).toHaveBeenCalled();
+    });
 });
