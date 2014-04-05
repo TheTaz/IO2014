a14fa7d9922e704307fcb5556ba45680f8cef008
TheTaz <tomasz.kozubowski@gmail.com>
2014-04-05 22:25:55 +0200

    Fixed errors.

--- utils/cs_to_js.sh
+++ utils/cs_to_js.sh
@@ -3,31 +3,10 @@
 #Deleting bin folder
 rm -rf bin/
 
-#Getting CS filenames
-cs_filename=$(ls -t "src/*.coffee")
+#Copying js and html files to bin folder
+cp -R ./src ./bin
 
 #Compiling CS files to JS
-if [ -z "$cs_filename" ]; then
-	printf "\n:: WARN :: No CS files to compile !\n"
-else 
-	coffee -c $cs_filename
-fi
+coffee --compile --output ./bin/ ./src/
 
-#Creating new bin folder
-mkdir bin
-
-#Copying js and html files to bin folder
-js_files=$(find src/ -name "*.js")
-html_files=$(find src/ -name "*.html")
-
-if [ -z "$js_files" ]; then
-	printf "\n:: WARN :: No JS files to copy !\n"
-else
-	cp -t bin/ $js_files
-fi
-if [ -z "$html_files" ]; then
-	echo "\n:: WARN :: No HTML files to copy !\n"
-else
-	cp -t bin/ $html_files
-fi
 
