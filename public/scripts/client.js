0ef48083a752aa4da2450b401401e4c34c7b9c72
Przemek Burczyk <przemek.burczyk@sigmapoint.co>
2014-05-03 12:07:29 +0200

    Implemented communication example.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,12 @@
+console.log("Client script loaded, connecting to server...");
+
 var socket = io.connect('http://localhost/client');
 var lastServerMsgId = 0;
 var lastClientMsgId = 0;
 var tasks = {};
-//tasks = { 1: { task: "{ ... }" , results: { 1: ..., 2: ... } }, 2: { ... }}
+//tasks = { 1: { task: "{ ... }" , results: { 1: ..., 2: ... } }, 2: { ... } }
 socket.on('operation', function (operation) {
-	console.log("Received control message: " + operation);
+	console.log("Received control message with operation code: " + operation.opcode);
 
 	//TODO: Is this check needed?
 	if (lastServerMsgId + 1 != operation.msgId) {
@@ -18,15 +20,20 @@ socket.on('operation', function (operation) {
 
 	switch (operation.opcode) {
 		case 1:
-			//Add new task code
+			//Add new task code,
 
-			if(operation.data.runFun instanceof String){
+			console.log(operation.data.runFun);
+			console.log(typeof operation.data.runFun);
+
+			if(operation.data.runFun instanceof String || typeof operation.data.runFun === "string"){
 				taskId = operation.data.taskId;
 				tasks[taskId] = {};
 				tasks[taskId].task = operation.data.runFun;
+				tasks[taskId].results = {};
 				console.log("Added new task: " + tasks[taskId].task);
 				socket.emit("ack", { ack: true, msgId: operation.msgId});
 			} else {
+				console.log("No task function provided");
 				socket.emit("error", { error: 1, msgId: operation.msgId, details: { taskId: operation.data.taskId } });
 			}
 
@@ -38,7 +45,7 @@ socket.on('operation', function (operation) {
 
 			//TODO: Fire-and-forget call, or notify server that taskId was already removed?
 			delete tasks[taskId]
-
+			console.log("Deleted task: " + taskId);
 			socket.emit("ack", { ack: true, msgId: operation.msgId});
 
 			break;
@@ -52,12 +59,17 @@ socket.on('operation', function (operation) {
 			task = tasks[taskId];
 
 			if(task === undefined){
+				console.log("Cannot execute task, it doesn't exist")
 				socket.emit("error", { error: 1, msgId: operation.msgId, details: { taskId: operation.data.taskId } });
 				break;
 			}
 
+			console.log("Executing task: " + taskId);
+			console.log("Task code: " + task.task);
+			console.log("Job arguments: " + jobArgs);
 			result = eval(task.task).taskProcess(jobArgs);
 			task.results[jobId] = result;
+			console.log("Result is: " + result);
 
 			socket.emit("ack", { ack: true, msgId: operation.msgId});
 
--- src/server.coffee
+++ src/server.coffee
@@ -22,7 +22,18 @@ initializeServer = () ->
 
 initializeConnectionManager = () ->
   connectionManager.onPeerConnected (socket) ->
-    console.log("New user connected")
+  	console.log("New user connected")
+
+  	socket.on "ack", (data) ->
+  		console.log("Client accepted msgId: " + data.msgId)
+
+  	socket.on "jobResult", (data) ->
+  		console.log("Received response: " + data.data.jobResult)
+  	
+  	socket.emit("operation", {opcode: 1, msgId: 1, data: { taskId: 1, runFun: "({taskProcess: function(inputObj) { return [inputObj.number, inputObj.begin, inputObj.end]}})" } })
+  	#socket.emit("operation", {opcode: 2, msgId: 2, data: { taskId: 1 } })
+  	socket.emit("operation", {opcode: 3, msgId: 2, data: { taskId: 1, jobId: 1, jobArgs: { number: 999999, begin: 2, end: 999999 } } })
+
 
 initializeAdminConsole = () ->
   AdminConsole = require './admin_console'
